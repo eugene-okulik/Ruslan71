768329d9df732186df4b049589a6b5f54c08715c
Ruslan Bochin <ruslan.bochin071@gmail.com>
2025-10-30 17:21:39 +0300

    ruslan_bochin_homework_5

---
+++ homework/ruslan_bochin/homework_5/homework_5_2.py
@@ -0,0 +1,7 @@
+one = int(42) + 10
+two = int(514) + 10
+three = int(9) + 10
+
+print(one)
+print(two)
+print(three)
---
+++ homework/ruslan_bochin/homework_5/homework_5_3.py
@@ -0,0 +1,8 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+text = 'Students ' + ', '.join(students) + ' study thense subjects: ' + ', '.join(subjects)
+print(text)
+
+
+
